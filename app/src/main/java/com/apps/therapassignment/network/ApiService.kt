73b3592a69e1f6fe0136005194e25d972c6f0589
MD SHAH SAMIR <mdshahsamir@gmail.com>
2022-12-08 16:31:13 +0600

    request header removed from productlist api call

--- app/src/main/java/com/apps/therapassignment/network/ApiService.kt
+++ app/src/main/java/com/apps/therapassignment/network/ApiService.kt
@@ -2,10 +2,9 @@ package com.apps.therapassignment.network
 
 import com.apps.therapassignment.model.ProductListResponseItem
 import retrofit2.http.GET
-import retrofit2.http.Header
 
 interface ApiService {
 
     @GET("products")
-    suspend fun getProducts(@Header("Secret_Code") header: String = "e92d1aa5f67ce24713cf638550f5daa84ef5ea3466ae29af8b1ad16fbe6c5fbb"): List<ProductListResponseItem>
+    suspend fun getProducts(): List<ProductListResponseItem>
 }
